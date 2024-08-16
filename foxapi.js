c03cd06fb9bbdd8ed170e9a0e70e93cc5e1d6bff
Acost4 <viniacosta7@gmail.com>
2024-08-16 18:26:39 -0300

    Initial fecth api

---
+++ foxapi.js
@@ -0,0 +1,15 @@
+function buscarImagem(){
+    return fetch ("https://randomfox.ca/floof/?ref=public_apis",{
+        method: 'get'
+    })
+        .then(response => response.json())
+        .then(data => data.image)
+        .then((foxImage) => {const imgElement = document.getElementById("ifox");
+                imgElement.src = foxImage;
+                imgElement.style.display = "block"; })}
+    
+    
+        
+
+    
+ 
